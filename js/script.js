02eda720733b5684bfffca1bb1922485a39c4e07
cassandraerin <caseyerin1994@ufl.edu>
2016-11-04 18:08:37 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// demonstrates DOM image swapping
+Playing.onclick = function pictureChange() {
+	document.getElementById("Cats").src = "images/Playing.jpg";
+}
+Eating.onclick = function pictureChange() {
+	document.getElementById("Cats").src = "images/Eating.jpg";
+}
+Sleeping.onclick = function pictureChange() {
+	document.getElementById("Cats").src = "images/Sleeping.jpg";
+}
+Drinking.onclick = function pictureChange() {
+	document.getElementById("Cats").src = "images/Drinking.jpg";
+}
